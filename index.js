35f07bee057a02941e0128322b428c3041007d85
navinmanohar <navinmanohar78086@gmail.com>
2023-10-27 15:56:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+a="hello";
+b=10;  //integer
+// automatic 
+let c="hello";
+const d="hello";
+var e="hello";  //string
+let cd=10.5    //float
+// firstName="";
+// console.log(typeof cd)
+
+// document.write("hello");
+let _ef=10;
+$sum=10;
+// console.log($sum)
+
+let num=9;
+// if(num%2==0)   //true
+// {
+//     console.log("even")
+//     console.log("something other")
+// }
+// else if(num%4==0)
+//   console.log("divisor of threee")
+// else
+// console.log("odd");
+
+
+// {
+//     var a=10;
+//     let nam="Navin";
+//        nam="hello";
+//     const max=11*10;
+//         max=3*4;
+//     console.log(a)
+//     console.log(nam)
+//     console.log(max)
+// }
+//    console.log(a)
+    // console.log(nam)
+    // console.log(max)
+
+    // initilization
+    // for(;conditon;)
+    //  incriment 
+    // var i=1
+    // for( ;i<=10;)
+    // {
+    //     console.log(i)
+    //     i++;
+    // }
+    // var i=1
+    // while(i<=10)
+    // {
+    //     if(i%2==0)
+    //      console.log(i)
+    //     i++;
+    // }
+    // console.log(i);
+   
+
+    function num1(a,b){
+        return a*b;
+}
+    // console.log("body"+num1)
+//    let ch= num1(1)
+//    console.log(ch)    
+//    console.log(num2);  //print 
+
+   var num2=() =>  10; //define arrow function
+   
+//    console.log(num2()) // after define call arrow function
+
+//   console.log(typeof num2)
+  
+//    console.log(num3());  //normal function 
+   
+   function num3 (){   //definition of normal function
+    return 10;
+   }
+   
+//    console.log(num3())  //calling normal function
+
+
+// let arr=[1,2,3,"hello",10.5]
+// console.log(arr)
+
+// let person={
+//     firstName:"Navin",
+//     age:34,
+//     address:{
+//         dist:'noida',
+//         state:'u.p'
+//     },
+//     display:function displayName()
+//     {
+//         console.log(this.address.state);
+//     }
+// }
+// person.display()
+
+
+// arr.push(10)
+// console.log(arr.pop())
+// let ar=[20,11]
+// arr.length=0;
+// arr[9]=10;
+// console.log(arr[0])
+
+function fun(f){
+    console.log("hello i am callback "+console.log(10))
+
+}
+
+
+function fun1()
+{
+    console.log(40*4);
+}
+// fun(fun1);
+
+
+// for(let a of arr)
+// console.log(a)
+let arr=[1,2,3,4,5,6,7,8]
+arr.set(5,10)
+let array=arr.map((fkjghl)=>{
+    return fkjghl%2==0;
+})
+
+// let arr3=arr.filter((tell)=>{
+//     if(tell%2==0)
+//     return tell;
+// })
+// console.log(arr);
+
+
+
+// function f(dfhksjhgskjg)
+// {
+//     return dfhksjhgskjg;
+// }
+// f(9485636487)
